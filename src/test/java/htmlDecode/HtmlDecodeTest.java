9a3bac913ae5922dc0f761d5a0ab1d1076f8b1f9
morlakdzuu <xerandom@mail.ru>
2019-03-20 17:27:15 +0300

    + htmlDecode tests were fixed

--- src/test/java/htmlDecode/HtmlDecodeTest.java
+++ src/test/java/htmlDecode/HtmlDecodeTest.java
@@ -6,9 +6,14 @@ public class HtmlDecodeTest {
 
     @Test
     public void htmlDecode() {
-        assertEquals("\"'\"", HtmlDecode.htmlDecode("&quot;&apos;&quot;"));
-        assertEquals("Cat <says> \"Meow\". M&M's", HtmlDecode.htmlDecode("Cat &lt;says&gt; &quot;Meow&quot;. M&amp;M&apos;s"));
+        assertEquals("\"", HtmlDecode.htmlDecode("&quot;"));
+        assertEquals("'", HtmlDecode.htmlDecode("&apos;"));
+        assertEquals("<", HtmlDecode.htmlDecode("&lt;"));
+        assertEquals(">", HtmlDecode.htmlDecode("&gt;"));
+        assertEquals("&", HtmlDecode.htmlDecode("&amp;"));
         assertEquals("", HtmlDecode.htmlDecode(""));
         assertEquals("hello", HtmlDecode.htmlDecode("hello"));
+        assertEquals("\"'<>&", HtmlDecode.htmlDecode("&quot;&apos;&lt;&gt;&amp;"));
+        assertEquals("Cat <says> \"Meow\". M&M's", HtmlDecode.htmlDecode("Cat &lt;says&gt; &quot;Meow&quot;. M&amp;M&apos;s"));
     }
 }
