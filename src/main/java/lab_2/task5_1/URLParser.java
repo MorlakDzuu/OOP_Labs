4e6e3556ee0948ebb37c84e073c8e4fa083db82f
morlakdzuu <xerandom@mail.ru>
2019-04-04 18:18:49 +0300

    + url parser

--- src/main/java/lab_2/task5_1/URLParser.java
+++ src/main/java/lab_2/task5_1/URLParser.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class URLParser {
 
     public static void main(String[] args) {
-        URL url = new URL("https://www.youtube.com/watch?v=IWTvgZVWeB4");
-        String metaString = url.getMeta();
-        if (!metaString.equals(null)) {
-            System.out.println(metaString);
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("Введите url (для выхода из программы введите \"...\")");
+        String inputString = inputScanner.nextLine();
+        while (!inputString.equals("...")) {
+            URL url = new URL(inputString);
+            String metaString = url.getMeta();
+            if (metaString != null) {
+                System.out.println(metaString);
+            } else {
+                System.out.println("Неправильный вид url");
+            }
+            inputString = inputScanner.nextLine();
         }
+        System.exit(0);
     }
 }
