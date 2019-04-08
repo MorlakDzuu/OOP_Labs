2fd858542ad8b47af10d43b582e203361c902809
morlakdzuu <xerandom@mail.ru>
2019-04-08 17:16:32 +0300

    добавлены новые тесты

--- src/test/java/lab2/URLParser/URLTest.java
+++ src/test/java/lab2/URLParser/URLTest.java
@@ -1,36 +1,71 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class URLTest {
 
     @Test
-    public void getMeta() {
-        URL url = new URL("");
-        assertEquals(url.getMeta(), null);
+    public void getHost() {
+        assertEquals("google.com", URLParser.getHost("https://google.com"));
+        assertEquals(null, URLParser.getHost("://"));
+        assertEquals(null, URLParser.getHost("://:"));
+        assertEquals(null, URLParser.getHost(":///"));
+        assertEquals("localhost", URLParser.getHost("://localhost/"));
+        assertEquals("localhost", URLParser.getHost("://localhost:8080/"));
+        assertEquals("localhost", URLParser.getHost("://localhost:/"));
+        assertEquals(null, URLParser.getHost("localhost"));
+        assertEquals(null, URLParser.getHost(":localhost"));
+        assertEquals(null, URLParser.getHost("/localhost"));
+        assertEquals(null, URLParser.getHost("//localhost"));
+        assertEquals(null, URLParser.getHost(":/localhost"));
+        assertEquals(null, URLParser.getHost("http://:1/1.txt"));
+    }
 
-        url = new URL("hello");
-        assertEquals(url.getMeta(), null);
+    @Test
+    public void getProtocol() {
+        assertEquals(null, URLParser.getProtocol("http"));
+        assertEquals(null, URLParser.getProtocol("http:"));
+        assertEquals(null, URLParser.getProtocol("http/"));
+        assertEquals(null, URLParser.getProtocol("http:/"));
+        assertEquals(null, URLParser.getProtocol("http//"));
+        assertEquals(null, URLParser.getProtocol("htp://"));
+        assertEquals(null, URLParser.getProtocol("://"));
+        assertEquals(null, URLParser.getProtocol("p://"));
+        assertEquals(null, URLParser.getProtocol("ftphttp://"));
+        assertEquals(Protocol.HTTP, URLParser.getProtocol("http://"));
+        assertEquals(Protocol.HTTPS, URLParser.getProtocol("https://"));
+        assertEquals(Protocol.FTP, URLParser.getProtocol("ftp://"));
+    }
 
-        url = new URL("hps://javarush.ru/groups/posts/615-idea-hot-keys");
-        assertEquals(url.getMeta(), null);
+    @Test
+    public void getPort() {
+        assertEquals(null, URLParser.getPort("http://localhost:"));
+        assertEquals(null, URLParser.getPort(":/"));
+        assertEquals(null, URLParser.getPort("://localhost"));
+        assertEquals(null, URLParser.getPort("htp://localhost"));
+        assertEquals(null, URLParser.getPort("://"));
+        assertEquals(null, URLParser.getPort("://:"));
+        assertEquals("80", URLParser.getPort("http://localhost"));
+        assertEquals("443", URLParser.getPort("https://localhost"));
+        assertEquals("21", URLParser.getPort("ftp://localhost"));
+        assertEquals("5050", URLParser.getPort("ftp://localhost:5050"));
+        assertEquals("5050", URLParser.getPort("ftp://localhost:5050/man"));
+        assertEquals("50", URLParser.getPort(":50"));
 
-        url = new URL("https:/javarush.ru/groups/posts/615-idea-hot-keys");
-        assertEquals(url.getMeta(), null);
+        assertEquals(null, URLParser.getPort(":0"));
+        assertEquals("1", URLParser.getPort(":1"));
+        assertEquals("65535", URLParser.getPort(":65535"));
+        assertEquals(null, URLParser.getPort(":65536"));
+        assertEquals(null, URLParser.getPort(":-50"));
+        assertEquals(null, URLParser.getPort(":500000000000000000000"));
+    }
 
-        url = new URL("https/javarush.ru/groups/posts/615-idea-hot-keys");
-        assertEquals(url.getMeta(), null);
-
-        url = new URL("httpsjavarush.ru/groups/posts/615-idea-hot-keys");
-        assertEquals(url.getMeta(), null);
-
-        url = new URL("https://javarush.ru/groups/posts/615-idea-hot-keys");
-        assertEquals(url.getMeta(), "HOST : javarush.ru\nPORT : 443\nDOC : groups/posts/615-idea-hot-keys");
-
-        url = new URL("http://javarush.ru/groups/posts/615-idea-hot-keys");
-        assertEquals(url.getMeta(), "HOST : javarush.ru\nPORT : 80\nDOC : groups/posts/615-idea-hot-keys");
-
-        url = new URL("ftp://javarush.ru/groups/posts/615-idea-hot-keys");
-        assertEquals(url.getMeta(), "HOST : javarush.ru\nPORT : 21\nDOC : groups/posts/615-idea-hot-keys");
+    @Test
+    public void  getDoc() {
+        assertEquals(null, URLParser.getDoc("https://localhost"));
+        assertEquals(null, URLParser.getDoc("https://localhost/"));
+        assertEquals("good", URLParser.getDoc("https://localhost/good"));
+        assertEquals("g", URLParser.getDoc("https://localhost/g"));
+        assertEquals("good/luck/", URLParser.getDoc("https://localhost/good/luck/"));
     }
 }
